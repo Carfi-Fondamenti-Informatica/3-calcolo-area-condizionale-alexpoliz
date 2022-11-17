448226b148f3f4b06dbf6b0392b9333de2254794
alexpoliz <117836399+alexpoliz@users.noreply.github.com>
2022-11-17 16:40:07 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,46 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-   return 0;
+  float a=0, b=0, x=0;
+            int opzione=0;
+cin>>a>>b>>opzione;
+switch (opzione) {
+    case 0:
+        x = (a * b) / 2;
+        cout << x << endl;
+        break;
+        case 1:
+                x = (a * a);
+        cout << x << endl;
+        break;
+        case 2:
+                x = (a * b);
+        cout << x << endl;
+        break;
+
+    default:
+        cout << "opzione non valida" << endl;
 }
+      return 0;
+}
+
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+ 
